5391fe5821a3d26e80dc93cff6e4757cae10b5b2
Archerist <yunuscemberci@gmail.com>
2022-05-18 20:34:46 +0300

    routing is done

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,41 @@
 	<div class="common-layout">
 	<el-container>
 		<el-header>
-			<el-menu mode="horizontal" :ellipsis="true" id="nav">
-				<el-menu-item index="1">Profile</el-menu-item>
-				<el-sub-menu index="2">
+			<el-menu mode="horizontal" id="nav" @select="handleSelect">
+				<el-menu-item index="/">Profile</el-menu-item>
+				<el-sub-menu index="/tools">
 					<template #title>
 						Tools
 					</template>
-					<el-menu-item>JWT</el-menu-item>
+					<el-menu-item index="/tools/jwt">JWT</el-menu-item>
 				</el-sub-menu>
 			</el-menu>
 		</el-header>
 		<el-main>
-			<div>
+			<el-container>
 				<router-view />
-			</div>
+			</el-container>
 		</el-main>
 	</el-container>
 	</div>
 </template>
+
+<script lang="ts">
+
+import { MenuItemClicked } from "element-plus";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name:"App",
+	methods:{
+		handleSelect : function (index:string) {
+			this.$router.push(index)
+		}
+	}
+})
+
+
+</script>
 
 <style>
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,25 +2,25 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Index from "../views/Index.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Index,
-  },
-  /* {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about"  "../views/About.vue"),
-  }, */
+	{
+		path: "/",
+		name: "Home",
+		component: Index,
+	},
+	{
+		path: "/tools/jwt",
+		name: "JWT",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "JWT" */ "../views/JWT.vue"),
+	}, 
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+	history: createWebHashHistory(),
+	routes,
 });
 
 export default router;
