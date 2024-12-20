35a573715e11dd782220abeec441c4e97ca139a8
Archerist <yunuscemberci@gmail.com>
2024-12-20 22:34:38 +0300

    slug page for blog content

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,8 +1,28 @@
 <template>
-  <main>
-    <ContentDoc :path="`${$route.path.replace(/\/blog(\/|)/,'/')}`" />
-  </main>
+    <ContentDoc :path="`${$route.path.replace(/\/blog(\/|)/,'/')}`">
+      <template #not-found>
+        <ContentNavigation v-slot="{navigation}">
+          <ul>
+        <li v-for="link of navigation" :key="link._path">
+          <NuxtLink :to="`/blog${link._path}`">{{ link.title }}</NuxtLink>
+        </li>
+      </ul>
+
+        </ContentNavigation>
+      </template>
+
+      <template v-slot="{ doc }">
+        
+        <UCard>
+          <template #header>{{ doc.title }}</template>
+          <ContentRenderer :value="doc" />
+
+        </UCard>
+      </template>
+      
+    </ContentDoc>
 </template>
+
 <script setup lang="ts">
 
 </script>
