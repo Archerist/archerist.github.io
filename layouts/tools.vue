a6a2e7a244a63a06142d86448e055ede4b5aa7fd
Archerist <yunuscemberci@gmail.com>
2024-12-20 22:34:30 +0300

    create layout for tools

---
+++ layouts/tools.vue
@@ -0,0 +1,20 @@
+<template>
+    <NuxtLayout :name="'default'">
+        <UCard>
+            <template #header>
+                <UHorizontalNavigation :links="nav"></UHorizontalNavigation>
+            </template>
+            <slot />
+        </UCard>
+    </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+
+const nav: Ref<any[]> = ref([{
+  label: 'JWT',
+  to: '/tools'
+}])
+
+</script>
+
