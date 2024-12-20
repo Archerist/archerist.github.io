8f64071e007f3993ed76913cb1b921b73d1bce01
Archerist <yunuscemberci@gmail.com>
2024-12-20 22:34:21 +0300

    create default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,25 @@
 <template>
-    <UHorizontalNavigation :links="mainNav"></UHorizontalNavigation>
-    <slot/>
+    <UContainer :ui="{constrained:''}">
+        <UHorizontalNavigation :links="mainNav"></UHorizontalNavigation>
+        <slot />    
+    </UContainer>
 </template>
-
+ 
 
 <script setup lang="ts">
 
 const mainNav: Ref<any[]> = ref([{
+  label: 'Profile',
+  icon: 'i-heroicons-user',
+  to: '/'
+}, {
+  label: 'Blog',
+  icon: 'i-heroicons-newspaper',
+  to: '/blog'
+}, {
+  label: 'Tools',
+  icon: 'i-heroicons-wrench-screwdriver',
+  to: '/tools'
 
 }])
 
