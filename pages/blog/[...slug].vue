6276b5afa02b1127140d00394a4ab9852ffdc3c3
Archerist <yunuscemberci@gmail.com>
2024-12-20 22:44:33 +0300

    fix prerendering

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,14 +1,7 @@
 <template>
     <ContentDoc :path="`${$route.path.replace(/\/blog(\/|)/,'/')}`">
       <template #not-found>
-        <ContentNavigation v-slot="{navigation}">
-          <ul>
-        <li v-for="link of navigation" :key="link._path">
-          <NuxtLink :to="`/blog${link._path}`">{{ link.title }}</NuxtLink>
-        </li>
-      </ul>
-
-        </ContentNavigation>
+        
       </template>
 
       <template v-slot="{ doc }">
