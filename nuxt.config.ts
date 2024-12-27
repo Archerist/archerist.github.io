a3def7bc878ada5c0e7c9067ed80b884572d82a7
Archerist <yunuscemberci@gmail.com>
2024-12-27 19:25:44 +0300

    remove navigation fields

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,6 @@ export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/ui'],
 
   ssr:true,
-  content:{
-    navigation:{
-      fields: ["category", "date", "brief"]
-    }
-  },
 
   compatibilityDate: '2024-12-20'
 })
